0d8b64c3c9b6861de1c4515a283428e883953a4b
Dmitry Belyaev <dmitry.belyaev@booking.com>
2021-12-27 21:57:46 +0100

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as s from "./index.module.css";
+
+const App = () => <div className={s.root}>Hey Parcel!</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
